25c0f4f5e066c1ef7bce476a9579919529e8c896
Abdur-Rawf <rawf.ict20@gmail.com>
2026-01-15 19:52:00 +0600

    Create Account.java

---
+++ OOP Revisiting - 9 Projects/PROJECT–4: Encapsulation/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount > 0)
+            balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
