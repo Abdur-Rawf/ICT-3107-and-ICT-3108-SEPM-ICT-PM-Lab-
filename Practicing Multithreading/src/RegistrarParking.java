d224094260fb22854f4078a8a67666f01d0c3eef
Abdur-Rawf <rawf.ict20@gmail.com>
2026-01-18 01:03:38 +0600

    Create RegistrarParking.java

---
+++ Practicing Multithreading/src/RegistrarParking.java
@@ -0,0 +1,13 @@
+public class RegistrarParking {
+
+    private static int counter = 1;
+    private int parkingId;
+
+    public RegistrarParking() {
+        this.parkingId = counter++;
+    }
+
+    public int getParkingId() {
+        return parkingId;
+    }
+}
